0885eda681f48d70cc8b17ff73acb3562f3356b4
paysonwallach <paysonwallach@icloud.com>
2018-10-15 10:44:50 +0200

    Initial Commit

---
+++ Duplicate Tab/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Duplicate Tab
+//
+//  Created by Payson Wallach on 10/15/18.
+//  Copyright © 2018 Payson Wallach. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
