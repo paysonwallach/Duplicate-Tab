5b26094db8dcee931dea886b7b07e1b6a2a7647c
Payson Wallach <paysonwallach@icloud.com>
2019-07-31 07:57:15 -0700

    Added setup view

---
+++ Duplicate Tab/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Duplicate Tab
+//
+//  Created by Payson Wallach on 10/16/18.
+//  Copyright © 2018 Payson Wallach. All rights reserved.
+//
+
+import Cocoa
+
+extension NSColor {
+    static let pageIndicator = NSColor.init(named: "pageIndicator")
+    static let currentPageIndicator = NSColor.init(named: "currentPageIndicator")
+}
